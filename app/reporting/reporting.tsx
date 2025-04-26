96b162ca9bd8b587dd8016b68bb201ce80dd10c7
sarahbarberuk <sarahbarberuk@gmail.com>
2025-04-26 19:34:30 +0100

    Added bare bones of reporting feature

--- app/reporting/reporting.tsx
+++ app/reporting/reporting.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Reporting: React.FC = () => {
   return (
     <div>
-      <h1>Reports</h1>
+      <h1>Reports 123</h1>
       <p>Placeholder for a reporting section</p>
     </div>
   );
